9eb63365f1f67b63eeeb500ec5abd74e739c6644
nnei <kektheboi@gmail.com>
2024-11-03 19:22:49 +0100

    COMMIT

---
+++ examples/inputcallfuncs.e
@@ -0,0 +1,9 @@
+function add_numbers
+    input a
+    input b
+    add result a b
+    print "The result of a + b is:"
+    print result
+end
+
+call add_numbers
--- src/elang.cpp
+++ src/elang.cpp
@@ -3,15 +3,69 @@
 #include <unordered_map>
 #include <sstream>
 #include <string>
+#include <vector>
+#include <stack>
 
 std::unordered_map<std::string, int> variables;
+std::unordered_map<std::string, std::vector<std::string>> functions;
+bool in_function = false;
+std::string current_function_name;
+
+void interpret_line(const std::string &line);
+
+void define_function(const std::string &func_name) {
+    current_function_name = func_name;
+    functions[func_name] = std::vector<std::string>();
+    in_function = true;
+}
+
+void interpret_script(const std::string &filename);
 
 void interpret_line(const std::string &line) {
-    std::istringstream iss(line);
+    std::string trimmed_line = line;
+    trimmed_line.erase(0, trimmed_line.find_first_not_of(" \t"));
+    trimmed_line.erase(trimmed_line.find_last_not_of(" \t") + 1);
+    if (trimmed_line.empty()) return;
+
+    std::istringstream iss(trimmed_line);
     std::string command;
     iss >> command;
 
-    if (command == "print") {
+    if (in_function) {
+        if (command == "end") {
+            in_function = false;
+        } else {
+            functions[current_function_name].push_back(trimmed_line);
+        }
+        return;
+    }
+
+    if (command == "call") {
+        std::string func_name;
+        iss >> func_name;
+        auto func_it = functions.find(func_name);
+        if (func_it != functions.end()) {
+            for (const auto &func_line : func_it->second) {
+                interpret_line(func_line);
+            }
+        } else {
+            std::cerr << "Error: Undefined function '" << func_name << "'" << std::endl;
+        }
+    }
+    else if (command == "function") {
+        std::string func_name;
+        iss >> func_name;
+        define_function(func_name);
+    }
+    else if (command == "input") {
+        std::string var_name;
+        iss >> var_name;
+        int value;
+        std::cout << "Enter value for " << var_name << ": ";
+        std::cin >> value;
+        variables[var_name] = value;
+    }
+    else if (command == "print") {
         std::string value;
         iss >> std::ws;
         char ch = iss.peek();
